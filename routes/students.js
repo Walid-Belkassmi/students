7a765117152da682f34ccc66e026570dd7b6259f
Walid <belkassmi.walid@gmail.com>
2022-08-09 21:51:39 +0200

    back done

--- routes/students.js
+++ routes/students.js
@@ -6,4 +6,14 @@ app.get("/", (req, res) => {
   res.json(studentsName);
 });
 
+app.post("/", (req, res) => {
+  const student = {
+    id: studentsName.length + 1,
+    name: req.body.name,
+  };
+
+  studentsName.push(student);
+  res.json(student);
+});
+
 module.exports = app;
