5e0261cdad6e7932df11bfb4be205f444b44cc88
Walid <belkassmi.walid@gmail.com>
2022-08-10 09:06:56 +0200

    student id

--- routes/students.js
+++ routes/students.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const studentsName = require("../studentsName");
 
+// Récupérer la liste complète
 app.get("/", (req, res) => {
   res.json(studentsName);
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const student = studentsName.find((student) => student.id === Number(id));
+
+  if (student) {
+    res.json(`${student.name}`);
+  } else {
+    res.status(404).send("Not found");
+  }
 });
 
 app.post("/", (req, res) => {
